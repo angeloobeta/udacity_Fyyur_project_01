326e564b90fe2fc887a6a7746170613f8c1bb59f
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-06-22 04:29:30 +0100

    Drop & create database

--- app1.py
+++ app1.py
@@ -2,16 +2,16 @@
 # Imports
 # ----------------------------------------------------------------------------#
 
-import json
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from logging import Formatter, FileHandler
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_moment import Moment
 import dateutil.parser
 import babel
-from flask_migrate import Migrate
 import config
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
-from logging import Formatter, FileHandler
+import json
 from flask_wtf import Form
 from forms import *
 
