67953a47b85aa6b671e498b5fd33df4c133886f4
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-06-25 15:58:15 +0100

    Created and moved models to model.py file; created the constructor for each model (Venue, Artists, Shows).

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
-from model import Venue, Artist, Shows, db
+from model import Venue(), Artist, Shows, db
 import config
 from forms import *
 
